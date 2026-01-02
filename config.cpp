ce5771197251ed756ea9c3e3eb58c44b1fd1a986
ShadyOwl <152045089+ShadyOwl@users.noreply.github.com>
2026-01-01 22:11:41 -0600

    Uniform pattern done.
    Kill me.

--- config.cpp
+++ config.cpp
@@ -3,7 +3,7 @@ class CfgPatches
 {
  class TPW_Extended_Arsenal
  {
-	requiredaddons[] = {"aceax_main", "ace_arsenal", "aceax_gearinfo"};
+	requiredaddons[] = {"aceax_main", "ace_arsenal", "aceax_gearinfo","ADPM","AMCU","AMCUF","AMP","AOR1","AOR2","ARID","ARMATA","ATACS","BDC","BLC","BME","BROWN","CADPAT","CCE","CDW","CHIP","CONCAMO","CROPAT","DARK","DCCU","DPCU","DPDU","DPM","EMR","ESE","FLECK","GLC","GPU","GRANITE","GZONE","ICAM","IDPM","IDT","ITMP","JEITAI","JIGSAW","KA2","KDC","KILO","LDF","M05","M09","M09D","M14","M2011","M2015","M81","M90","M98","MAD21","MANDRAKE","MARPAT","MARPATD","MCAM","MCAMA","MCAMD","MCAMF","MCAMT","MLAND","MMPU","MTARN","MTP","MWC","NFP","NFPT","NZMCU","NZMTP","OBH","OCP","OCPF","OD","PAP","PSDC","S2000","SASR","SLOCAM","SURPAT","T07D","TAC","TAN","TAT","TAZ90","TE3","TIGER","TIGERAT","TUNPAT","UNIPAT","US4CES","VEG","VSR","VZ2007","WLP","XINGKONG","Z93"};
 	units[] = {};
 	weapons[] = {""};
 	magazines[] = {""};
